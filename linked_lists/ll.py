73d6b9f7e739fa70c3bff15f4661572b06826447
Erik Farmer <devbook@Eriks-MacBook-Pro.local>
2015-07-09 13:47:41 -0700

    add methods to UnorderedList

--- linked_lists/ll.py
+++ linked_lists/ll.py
@@ -10,6 +10,31 @@ class UnorderedList(object):
     def is_empty(self):
         return self.head is None
 
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.next
+        return count
+
+    def search(self, data):
+        current = self.head
+        while current is not None and current.data != data:
+            current = current.next
+        return current
+
+    def remove(self, data):
+        previous, current = None, self.head
+        while current is not None:
+            if current.data == data:
+                break
+            previous, current = current, current.next
+        if previous is None:
+            self.head = current.next
+        else:
+            previous.next = current.next
+
 
 class Node(object):
 
